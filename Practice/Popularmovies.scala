501797cff26f2429227c2b030fb3b49e4dfeff54
dipodaimary <dipodaimary@gmail.com>
2018-05-03 23:16:25 +0530

    First Commit

--- Practice/Popularmovies.scala
+++ Practice/Popularmovies.scala
@@ -1,6 +1,29 @@
 import org.apache.spark._
 import org.apache.spark.SparkContext._
 import org.apache.log4j._
+import scala.io.Source
+import java.nio.charset.CodingErrorAction
+import scala.io.Codec
+
+def loadMovieNames() : Map[Int, String] = { 
+    // Handle character encoding issues:
+    implicit val codec = Codec("UTF-8")
+    codec.onMalformedInput(CodingErrorAction.REPLACE)
+    codec.onUnmappableCharacter(CodingErrorAction.REPLACE)
+
+    // Create a Map of Ints to Strings, and populate it from u.item.
+    var movieNames:Map[Int, String] = Map()
+    
+     val lines = Source.fromFile("u.item").getLines()
+     for (line <- lines) {
+       var fields = line.split('|')
+       if (fields.length > 1) {
+        movieNames += (fields(0).toInt -> fields(1))
+       }
+     }
+    
+     return movieNames
+  }
 
 // Set the log level to only print errors
 Logger.getLogger("org").setLevel(Level.ERROR)
