901cb97669c3ff533311521068bb6cab7107db01
dipodaimary <dipodaimary@gmail.com>
2018-05-04 23:17:14 +0530

    First Commit

---
+++ Tutorial/Basics.scala
@@ -0,0 +1,22 @@
+//Expressions
+1+1
+
+val two = 1+2
+
+//Variables
+var name = "steve"
+
+def addOne(m:Int):Int = {
+//Functions
+m+1
+}
+
+println(addOne(2))
+
+//Anonymous Functions
+(x:Int)=>x+1
+
+def timesTwo(i:Int):Int={
+println("Hello world")
+i*2
+}
