c3e7778adacf047690b4c2daaa493d5b7faf480b
dipodaimary <dipodaimary@gmail.com>
2018-05-05 13:44:06 +0530

    First Commmit

--- Tutorial/Basics_Continued.scala
+++ Tutorial/Basics_Continued.scala
@@ -57,3 +57,14 @@ times match {
   case i if i==2 => println("two")
   case _ => println("some other number")
 }
+
+//Matching on type
+def bigger(o:Any):Any = {
+  o match {
+    case i:Int if i<0 => i-1
+    case i:Int => i+1
+    case d:Double if d<0.0 =>d-0.1
+    case d:Double =>d+0.1
+    case text: String => text+"s"
+  }
+}
