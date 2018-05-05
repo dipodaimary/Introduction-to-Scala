4a6f8836e0013feeb74989b92b90d7e247da3e9e
dipodaimary <dipodaimary@gmail.com>
2018-05-05 23:00:21 +0530

    First Commit

--- Tutorial/Collections.scala
+++ Tutorial/Collections.scala
@@ -71,3 +71,34 @@ val result = list1.zip(list2)
 //partition splits a list based on where it falls with respect to the predicate function
 val numbers  = List(1,2,3,4,5,6,7,8,9,10)
 numbers.partition((i:Int) => i%2==0)
+
+//Find
+numbers.find((i:Int)=>i>5) //alternatively numbers.find(_>5)
+
+//drop and dropWhile
+val droppedNumbers = numbers.drop(5) //drops first i the elements
+val droppedNumbers = numbers.dropWhile(_<3) //drop numbers with respect to the predicate function
+
+//foldLeft
+val temp  = numbers.foldLeft(0)((m:Int,n:Int)=>m+n)
+//visualize
+numbers.foldLeft(0) { (m: Int, n: Int) => println("m: " + m + " n: " + n); m + n }
+numbers.foldRight(0) { (m: Int, n: Int) => println("m: " + m + " n: " + n); m + n }
+
+//Flatten: flatten collapses one level of nested structure
+List(List(1,2),List(3,4)).flatten
+List(List(List(0,1),List(1,2)),List(3,4)).flatten
+
+//Flat Map
+val nestedNumbers = List(List(1, 2), List(3, 4))
+nestedNumbers.flatMap(x => x.map(_ * 2)) //short hand for nestedNumbers.map((x: List[Int]) => x.map(_ * 2)).flatten
+
+
+//Generalized Functional Combinators
+def ourMap(numbers:List[Int],fn:Int=>Int):List[Int] = {
+  numbers.foldRight(List[Int]()){
+    (x:Int,xs:List[Int])=>fn(x)::xs
+  }
+}
+def timesTwo(i:Int)={i*2}
+ourMap(numbers, timesTwo(_))
