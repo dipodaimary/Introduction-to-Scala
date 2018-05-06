287cf12c1813e2f43af9058786f161830bfc89d5
dipodaimary <dipodaimary@gmail.com>
2018-05-06 23:09:59 +0530

    First Commit

--- Tutorial/Spark02_RDD.scala
+++ Tutorial/Spark02_RDD.scala
@@ -1,8 +1,8 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 
-val conf = new SparkConf().setAppName(appName).setMaster(master)
-new SparkContext(conf)
+valconf = new SparkConf().setAppName(appName).setMaster(master)
+new SparkContextconf)
 
 //Paralallized collections
 val data = Array(1, 2, 3, 4, 5)
@@ -26,3 +26,19 @@ rd=>(word,1)).reduceByKey((count1,count2)=>count1+count2)
 
 println("Writing output to: notebooks/output/kjv-wc2")
 wc.saveAsTextFile("output/kjv-wc2")
+
+
+//Stand alone application
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+
+valconf = new SparkConf().setAppName("wordCount")
+val sc = new SparkContext(conf)
+
+val input = sc.textFile("data/apodat.txt")
+val words = input.flatMap(line=>line.split(","))
+
+//transform into pair and count
+val counts = words.map(word=>(word,1)).reduceByKey{case (x,y)=>x+y}
+counts.saveAsTextFile("output/wordCounts")
