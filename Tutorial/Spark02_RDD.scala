47b87a5269c92b1fd15e352c4140421aa0128bdb
dipodaimary <dipodaimary@gmail.com>
2018-05-06 23:30:55 +0530

    First Commit

--- Tutorial/Spark02_RDD.scala
+++ Tutorial/Spark02_RDD.scala
@@ -57,3 +57,9 @@ val result = input.aggregate((0, 0))(
 (acc, value) => (acc._1 + value, acc._2 + 1),
 (acc1, acc2) => (acc1._1 + acc2._1, acc1._2 + acc2._2))
 val avg = result._1 / result._2.toDouble
+
+//Persist
+val result = input.map(x => x * x)
+//import something? result.persist(StorageLevel.DISK_ONLY)
+println(result.count())
+println(result.collect().mkString(","))
