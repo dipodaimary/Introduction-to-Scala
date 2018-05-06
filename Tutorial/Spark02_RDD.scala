dd981c4708537aaf4a867306b7ea2df9a9b30a1d
dipodaimary <dipodaimary@gmail.com>
2018-05-06 23:27:22 +0530

    First Commit

--- Tutorial/Spark02_RDD.scala
+++ Tutorial/Spark02_RDD.scala
@@ -53,3 +53,7 @@ println(output.collect().mkString(", "))
 
 //Actions
 val sum = input.reduce((x,y)=>x+y)
+val result = input.aggregate((0, 0))(
+(acc, value) => (acc._1 + value, acc._2 + 1),
+(acc1, acc2) => (acc1._1 + acc2._1, acc1._2 + acc2._2))
+val avg = result._1 / result._2.toDouble
