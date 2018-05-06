8c9b2991bc25ed6be86595c33a079b7c6590671e
dipodaimary <dipodaimary@gmail.com>
2018-05-06 21:06:05 +0530

    First Commit

--- Tutorial/Spark02_RDD.scala
+++ Tutorial/Spark02_RDD.scala
@@ -11,3 +11,18 @@ val distData = sc.parallelize(data)
 val lines = sc.textFile("data.txt")
 val lineLengths = lines.map(s => s.length)
 val totalLength = lineLengths.reduce((a, b) => a + b)
+
+//Git tutorial
+val input = sc.textFile("data/kjvdat.txt").map(line => line.toLowerCase)
+input.cache
+
+val wc = input
+  .flatMap(line => line.split("""[^\p{IsAlphabetic}]+"""))  // RDD[String]
+  .map(word => (word, 1))                                   // RDD[Tuple2[String,Int]], a.k.a. RDD[(String,Int)]
+  .reduceByKey((count1, count2) => count1 + count2)         // RDD[(String,Int)]
+
+val wc = input.flatMap(line=>line.split("""[^\p{IsAlphabetic}]+""")).map(wo
+rd=>(word,1)).reduceByKey((count1,count2)=>count1+count2)
+
+println("Writing output to: notebooks/output/kjv-wc2")
+wc.saveAsTextFile("output/kjv-wc2")
