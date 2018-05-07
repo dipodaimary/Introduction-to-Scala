7aa73c1ff06c32e12520e8a94ffd79633b334891
dipodaimary <dipodaimary@gmail.com>
2018-05-07 22:07:25 +0530

    First Commit

---
+++ DSnAlgoScala/quickSort.scala
@@ -0,0 +1,23 @@
+def quicksort(xs:Array[Int]){
+    def swap(i:Int,j:Int){
+        xs(i) = xs(i)^xs(j)
+        xs(j) = xs(i)^xs(j)
+        xs(i) = xs(i)^xs(j)
+    }
+    def sort1(l:Int,r:Int){
+        val pivot = xs((l+r)/2)
+        var i = l; var j = r
+        while(i<=j){
+            while(xs(i)<pivot) i+=1
+            while(xs(j)>pivot) j-=1
+            if(i<=j){
+                swap(i,j)
+                i+=1
+                j-=1
+            }
+        }
+        if(l<j) sort1(l,j)
+        if(j<r) sort1(i,r)
+    }
+    sort1(0,xs.length-1)
+}
