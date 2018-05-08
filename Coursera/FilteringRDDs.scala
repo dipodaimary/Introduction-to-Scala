2aa56c200e353a3ac9fa3c01efb13c6fecb6d7a9
dipodaimary <dipodaimary@gmail.com>
2018-05-08 22:33:23 +0530

    First Commit

--- Coursera/FilteringRDDs.scala
+++ Coursera/FilteringRDDs.scala
@@ -18,3 +18,18 @@ for(result<-results){
     val formattedTemp = f"$temp%.2f F"
     println(s"$station minimum temperature: $formattedTemp")
 }
+
+//find out the date with most precipitation
+def parseLine2(line:String):(String,String,Int) = {
+    val fields = line.split(",")
+    val date = fields(1)
+    val entryType = fields(2)
+    val precipitation = fields(3).toInt
+    (date,entryType,precipitation)
+}
+val lines = sc.textFile("1800.csv")
+val parsedLines = lines.map(parseLine2)
+val percDates = parsedLines.filter(x=>x._2=="PRCP")
+val datePrecipitations = percDates.map(x=>(x._1,x._3))
+val dateWiseMaxPrecp = datePrecipitations.reduceByKey((x,y)=>if(x>y) x else y)
+dateWiseMaxPrecp.toDF("date","percp").sort(desc("percp")).collect().mkString("\n")
