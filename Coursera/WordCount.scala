ae1c825a242f6699d4b31c064266875c3d23cb47
dipodaimary <dipodaimary@gmail.com>
2018-05-08 23:07:57 +0530

    First Commit

--- Coursera/WordCount.scala
+++ Coursera/WordCount.scala
@@ -1,3 +1,10 @@
+
 val lines = sc.textFile("book.txt")
 val words = lines.flatMap(x=>x.split(" "))
 val wordCount = words.countByValue()
+
+//with regular expression
+val words = lines.flatMap(x=>x.split("\\W+"))
+val lowerCaseWords = words.map(x=>x.toLowerCase())
+
+val wordCount = lowerCaseWords.countByValue()
