738c4bea17f9b0bf67158e0a5ef7716398ba420d
dipodaimary <dipodaimary@gmail.com>
2018-05-08 23:14:53 +0530

    First Commit

--- Coursera/WordCount.scala
+++ Coursera/WordCount.scala
@@ -8,3 +8,13 @@ val words = lines.flatMap(x=>x.split("\\W+"))
 val lowerCaseWords = words.map(x=>x.toLowerCase())
 
 val wordCount = lowerCaseWords.countByValue()
+
+//instead of map we will create RDD
+val wordCount = lowerCaseWords.map(x=>(x,1)).reduceByValue((x,y)=>x+y)
+val wordCountSorted = wordCount.map(x=>(x._2,x._1)).sortByKey()
+wordCountSorted.foreach(println)
+for(result<-wordCountSorted){
+    val count = result._1
+    val word = result._2
+    println(s"$word: $count")
+}
