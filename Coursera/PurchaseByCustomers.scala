f2712bdd1932b501693878a2e1fce595a9599597
dipodaimary <dipodaimary@gmail.com>
2018-05-08 23:30:47 +0530

    First Commit

--- Coursera/PurchaseByCustomers.scala
+++ Coursera/PurchaseByCustomers.scala
@@ -1,5 +1,12 @@
 def parseLine(line:String):(Int,Float) = {
     val fields = line.split(",")
-    val customerId = fields(0)
-    val amountSpent = fields(2)
+    val customerId = fields(0).toInt
+    val amountSpent = fields(2).toFloat
+    (customerId,amountSpent)
 } 
+
+val input = sc.textFile("customer-orders.csv")
+val mappedInput = input.map(parseLine)
+val totalByCustomer = mappedInput.reduceByKey((x,y)=>x+y)
+totalByCustomer.collect()
+totalByCustomer.foreach(println)
