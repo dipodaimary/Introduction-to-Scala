840ec9733e6e823bbe80751db5345947502dc6ee
dipodaimary <dipodaimary@gmail.com>
2018-05-09 22:41:11 +0530

    First Commit

--- Coursera/DataFrameAndDataSets/SparkSQL.scala
+++ Coursera/DataFrameAndDataSets/SparkSQL.scala
@@ -2,6 +2,7 @@ import org.apache.spark._
 import org.apache.spark.SparkContext._
 import org.apache.spark.sql._
 import org.apache.log4j._
+import spark.implicits._
 
 object SparkSQL{
     case class Person(ID:Int, name:String, age:Int, numFriends:Int)
@@ -10,10 +11,14 @@ object SparkSQL{
         val person:Person = Person(fields(0).toInt, fields(1), fields(2).toInt, fields(3).toInt)
         return person
     }
-    def main(args:Array[String]){
+    def main(){//args:Array[String]
         //Set the log level to only print errors
         Logger.getLogger("org").setLevel(Level.ERROR)
         //Use new SparkSession interface in Spark 2.0
         val spark = SparkSession.builder.appName("SparkSQL").master("local[*]").getOrCreate()
+        val lines =spark.sparkContext.textFile("fakefriends.csv")
+        val people = lines.map(mapper)
+        //Infer the schema, and register the Dataset as a table
+        val schemaPeople = people.toDS
     }
 }
--- Coursera/SparkSQL.scala
+++ Coursera/SparkSQL.scala
@@ -2,10 +2,11 @@ import org.apache.spark._
 import org.apache.spark.SparkContext._
 import org.apache.spark.sql._
 import org.apache.log4j._
+import spark.implicits._
 
 object SparkSQL{
     case class Person(ID:Int, name:String, age:Int, numFriends:Int)
-    def mapper(line:String):Person = {
+    def mapper(line:String) : Person = {
         val fields = line.split(",")
         val person:Person = Person(fields(0).toInt, fields(1), fields(2).toInt, fields(3).toInt)
         return person
@@ -18,7 +19,8 @@ object SparkSQL{
         val lines =spark.sparkContext.textFile("fakefriends.csv")
         val people = lines.map(mapper)
         //Infer the schema, and register the Dataset as a table
-        import spark.implicits._
         val schemaPeople = people.toDS
+        val teenAgers = spark.sql("SELECT * FROM people WHERE age>=13 AND age<=19")
+        spark.stop()
     }
 }
