384ae3038193fb09aed599cc64c2c68c463d9921
dipodaimary <dipodaimary@gmail.com>
2018-05-09 22:57:43 +0530

    First Commit

---
+++ Coursera/DataFrameAndDataSets/DataFrames2.scala
@@ -0,0 +1,31 @@
+import org.apache.spark._
+import org.apache.spark.SparkContext._
+import org.apache.spark.sql._
+import org.apache.log4j._
+import spark.implicits._
+
+case class Person(ID:Int, name:String, age:Int, numFriends:Int)
+def mapper(line:String):Person = {
+    val fields = line.split(",")
+    val person:Person = Person(fields(0).toInt, fields(1), fields(2).toInt, fields(3).toInt)
+    return person
+}
+val spark = SparkSession.builder.appName("SparkSQL").master("local[*]").getOrCreate()
+val lines = spark.sparkContext.textFile("fakefriends.csv")
+val people = lines.map(mapper).toDS().cache()
+
+println("Here is the inferred schema")
+people.printSchema()
+
+people.select("name").show()
+
+//Filter anyone over 21
+people.filter(people("age")<21).show()
+
+//Group by
+people.groupBy("age").count().show()
+
+//Small mutations
+people.select(people("name"),people("age")+10).show()
+
+spark.stop()
