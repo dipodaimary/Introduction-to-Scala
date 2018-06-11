52f82cab56d2bd3aa7824171369d03422a1abc3e
dipodaimary <dipodaimary@gmail.com>
2018-06-11 16:22:24 +0530

    First Commit

--- Programmming in Scala/Currying.scala
+++ Programmming in Scala/Currying.scala
@@ -15,3 +15,13 @@ object Exercise{
     }
 }
 Exercise.product(x=>x*x)(3,4)
+
+//Map Reduce version
+object Exercise{
+    def mapReduce(f:Int=>Int, combine:(Int,Int)=>Int, zero:Int)(a:Int, b:Int):Int = {
+        if(a>b) zero
+        else combine(f(a), mapReduce(f, combine, zero)(a+1, b))
+    }
+    def product(f:Int => Int)(a:Int, b:Int):Int = mapReduce(f, (x,y)=>x*y,1)(a,b)
+}
+Exercise.product(x=>x*x)(3,4)
