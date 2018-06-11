861ce17d887943e59ff8c4cc3d9ea1ed510dead3
dipodaimary <dipodaimary@gmail.com>
2018-06-11 22:17:10 +0530

    First Commit

---
+++ Programmming in Scala/Classes1.scala
@@ -0,0 +1,7 @@
+package week3
+
+class Rational(x:Int, y:Int){
+    //require(y!=0,"denominator must be non-zero")
+    println(x+", "+y)
+    println("Hello from rational")
+}
---
+++ Programmming in Scala/FunctionsAndData.scala
@@ -0,0 +1,14 @@
+object rationals{
+    val x = new Rational(1,2)
+    println(x.numer)
+    println(y.denom)
+}
+
+class Rational(x:Int, y:Int){
+    def numer = x
+    def denom = y
+    def add(that: Rational) = new Rational(numer*that.denom + that.numer*denom,denom*that*denom)
+    def neg :Rational = new Rational(-numer,denom)
+    def sub(that:Rational) = add(that.neg)
+    override def toString = numer +"/" + denom
+}
