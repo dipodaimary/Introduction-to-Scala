1b022cddee8daa45cc3e29abeff0a5099900bfab
dipodaimary <dipodaimary@gmail.com>
2018-06-11 22:57:29 +0530

    First Commit

---
+++ Programmming in Scala/mergerSort.scala
@@ -0,0 +1,9 @@
+object mergesort{
+    def msort(xs:List[Int]): List[Int] = {
+        val n = xs.length/2
+        if(n==0) xs
+        else{
+            def merge(xs:List[Int], ys:List[Int]):List[Int] = (xs,xs)
+        }
+    }
+}
