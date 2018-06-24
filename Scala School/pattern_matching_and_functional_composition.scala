024c204406c8af8031ad3c47be8d499d9fa8ee96
dipodaimary <dipodaimary@gmail.com>
2018-06-25 00:20:22 +0530

    First Commit

--- Scala School/pattern_matching_and_functional_composition.scala
+++ Scala School/pattern_matching_and_functional_composition.scala
@@ -6,3 +6,25 @@ fComposeG("yay")
 
 val fAndThenG = f _ andThen g _
 fAndThenG("yay")
+
+
+//Currying vs Partial Functions
+val one: PartialFunction[Int,String] = {case 1 => "one"}
+one.isDefinedAt(1)
+one.isDefinedAt(2)
+one(1)
+val two: PartialFunction[Int,String] = {case 2 => "two"}
+two.isDefinedAt(1)
+two.isDefinedAt(2)
+two(2)
+
+val three: PartialFunction[Int,String] = {case 3 => "two"}
+
+val wildcard: PartialFunction[Int, String] = {case _ => "Something Else"}
+wildcard(3)
+
+val partial = one orElse two orElse three orElse wildcard
+partial(1)
+partial(2)
+partial(3)
+partial(4)
